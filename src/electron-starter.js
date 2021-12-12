207d3ec26624322c8f072014e8f63c7ee2a7bf81
Navrin <navman360@gmail.com>
2021-12-12 20:05:21 +1100

    Add error logging

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -153,3 +153,8 @@ app.on("activate", function() {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+process.on('uncaughtException', function (err) {
+    console.log(err);
+  })
+  
